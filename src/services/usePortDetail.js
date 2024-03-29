a7f05756e83f0d2f7849d0661c47e113901129a1
sanjai12 <43547825+sanjai12@users.noreply.github.com>
2024-03-29 14:18:53 +0000

    changed port api

--- src/services/usePortDetail.js
+++ src/services/usePortDetail.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 axios.defaults.baseURL =
-  'https://maslogistics-555196998.eu-north-1.elb.amazonaws.com/masquote/getPacknPortsByMode';
+  'https://maslogistics-555196998.eu-north-1.elb.amazonaws.com/api/getPacknPortsByMode';
 
 const usePortDetails = (mode = '') => {
   const [response, setResponse] = useState(null);
